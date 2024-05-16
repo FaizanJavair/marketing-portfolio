e23bf5a2a3535d9f23b9e44c3cb8fb257b792372
faizanjavair <faizanjavair@gmail.com>
2024-05-17 02:19:31 +0500

    testing on 14 Pro

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -54,9 +54,11 @@ export const About = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="empty-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Sales leader with <b>25+ years of experience</b>  in the telecom/ISP sector, specializing in Internet, Wireless, FTTX, Cloud, and Colocation. 
-          Expertise in networking equipment <b>(Cisco, Juniper)</b>  and IT market trends. 
-          Experienced in designing Corporate WAN, MAN, and Internet Networks, and implementing strategic projects for top-line and bottom-line impact.
+          I'm a skilled website developer with experience in TypeScript and
+          Javascript, and expertise in frameworks like React, Node.js, and
+          Three.js. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!
         </motion.p>
 
         {/* Service Card */}
